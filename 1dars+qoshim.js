2b22d177fe43a8098cb1bb6f4f714009498beaa9
UlugbekKhon <legendawoyni@gmail.com>
2026-01-15 16:44:22 +0500

    JS2DARS

--- 1dars+qoshim.js
+++ 1dars+qoshim.js
@@ -1,21 +1,82 @@
 
-const son111 = 10;
-const son222 = 5;
-const arifmetikAmal = "+"
-let matija;
+// const son111 = 10;
+// const son222 = 5;
+// const arifmetikAmal = "+"
+// let matija;
 
-switch ( arifmetikAmal ) {
-    case "+":
-        natija77 = son111 + son222
-        break;
-    case "-":
-        natija77 = son111 - son222  
-        break;
-    case "/":
-        natija77 = son111 / son222
-        break;
-    case "*":
-        natija77 = son111 * son222 
-        break;      
-}
-console.log (natija77)
+// switch ( arifmetikAmal ) {
+//     case "+":
+//         natija77 = son111 + son222
+//         break;
+//     case "-":
+//         natija77 = son111 - son222  
+//         break;
+//     case "/":
+//         natija77 = son111 / son222
+//         break;
+//     case "*":
+//         natija77 = son111 * son222 
+//         break;      
+// }
+// console.log (natija77)
+
+// const kun = 4;
+// let output;
+// switch (kun) {
+//  case 1:
+//  output = 'dushanba';
+//  break;
+//  case 2:
+//  output = 'Seshanba';
+//  break;
+//   case 3:
+//  output = 'Chorshanba';
+//  break;
+//   case 4:
+//  output = 'Payshanba';
+//  break;
+//  case 5:
+//  output = 'Juma';
+//  break;
+//  case 6:
+//  output = 'Shanba';
+//  break;
+//  case 7:
+//  output = 'Yakshanba';
+//  break;
+//   default:
+//  output = 'topilmadi';
+//  break;
+// }
+// console.log(`Kun: ${output}`);
+
+// for (let i = 0; i <= 20; i++) {
+//  if (i % 2 == 0)
+//  console.log(i);
+// }
+
+
+// let i = 0;
+// while (i < 10) {
+// i++;
+// console.log(i);
+// }
+
+// let i = 10;
+// while (i => 0) {
+// i--;
+// console.log(i);
+// }
+
+// let ik = 0; 
+// while (ik <= 20) {
+// if (ik % 2 == 0)
+//  console.log(ik);
+// ik++;
+// }
+
+// let i = 5;
+// do {
+// console.log(i);
+// i--;
+// } while (i >= 0)
